338c3f9dd4d030467eeeb586db960d859a846a3a
xXCarlosXx08 <130647173+xXCarlosXx08@users.noreply.github.com>
2023-05-22 08:33:41 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,53 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        if (mi_jugada == 1) {
+            basic.showString("Empate")
+        }
+    } else if (receivedNumber == 2) {
+        if (mi_jugada == 2) {
+            basic.showString("Perdedor")
+        }
+    } else if (receivedNumber == 1) {
+        if (mi_jugada == 3) {
+            basic.showString("Ganador")
+        }
+    } else if (receivedNumber == 2) {
+        if (mi_jugada == 1) {
+            basic.showString("Perdedor")
+        }
+    } else if (receivedNumber == 2) {
+        if (mi_jugada == 2) {
+            basic.showString("Empate")
+        }
+    } else if (receivedNumber == 2) {
+        if (mi_jugada == 3) {
+            basic.showString("Gandor")
+        }
+    } else if (receivedNumber == 3) {
+        if (mi_jugada == 3) {
+            basic.showString("Perdedor")
+        }
+    } else if (receivedNumber == 3) {
+        if (mi_jugada == 3) {
+            basic.showString("Gandor")
+        }
+    } else if (receivedNumber == 3) {
+        if (mi_jugada == 3) {
+            basic.showString("Empate")
+        }
+    }
+})
 input.onButtonPressed(Button.A, function () {
+    mi_jugada = randint(1, 1)
     basic.showIcon(IconNames.SmallSquare)
-    mi_jugada = 1
-    radio.sendNumber(1)
 })
 input.onButtonPressed(Button.AB, function () {
+    mi_jugada = randint(3, 3)
     basic.showIcon(IconNames.Scissors)
-    Mi_jugada_3 = 3
-    radio.sendNumber(3)
 })
 input.onButtonPressed(Button.B, function () {
+    mi_jugada = randint(2, 2)
     basic.showIcon(IconNames.Square)
-    Mi_jugada_2 = 2
-    radio.sendNumber(2)
 })
-let Mi_jugada_2 = 0
-let Mi_jugada_3 = 0
 let mi_jugada = 0
 radio.setGroup(107)
